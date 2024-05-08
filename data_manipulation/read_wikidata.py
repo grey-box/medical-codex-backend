f73d57cc205da5bf6e20c7d30f64cc4320d5bdca
François Pelletier <francois@jevalide.ca>
2024-05-08 01:10:08 -0400

    New wikidata extraction, script and prepared data. Added RxNorm to the extracted labels in the final table

--- data_manipulation/read_wikidata.py
+++ data_manipulation/read_wikidata.py
@@ -3,10 +3,10 @@ import pathlib
 import pandas as pd
 
 # %% Define path to raw data
-raw_data_path = (pathlib.Path(os.getcwd()) / "raw_data" / "wikidata" / "wikidata_names.csv")
+raw_data_path = (pathlib.Path(os.getcwd()) / "raw_data" / "wikidata" / "wikidata_names_2024-05-08.csv")
 
 # %% Read raw data
-raw_data = pd.read_csv(raw_data_path, sep=',', encoding="UTF-8")
+raw_data = pd.read_csv(raw_data_path, sep=',', encoding="UTF-8", dtype={'rxnorm': str, 'chebi_id': str})
 
 # %% Parse json data for Ukrainian names
 raw_data['alias_list_uk'] = raw_data['alias_list_uk'].apply(lambda x: eval(x))
@@ -14,3 +14,14 @@ raw_data['alias_list_uk'] = raw_data['alias_list_uk'].apply(lambda x: eval(x))
 raw_data['alias_list_ru'] = raw_data['alias_list_ru'].apply(lambda x: eval(x))
 # %% Parse json data for English names
 raw_data['alias_list_en'] = raw_data['alias_list_en'].apply(lambda x: eval(x))
+# %% Parse json data for French names
+raw_data['alias_list_fr'] = raw_data['alias_list_fr'].apply(lambda x: eval(x))
+
+# %% save data to csv file
+raw_data.to_csv(pathlib.Path(os.getcwd()) / "prepared_data" / "wikidata-names.csv",
+                quoting=2,
+                index=False)
+
+# %% save data to json file
+raw_data.to_json(pathlib.Path(os.getcwd()) / "prepared_data" / "wikidata-names.json",
+                  orient='records')
