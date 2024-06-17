a0418955213c9f073198068b3a5f52df597c86f2
François Pelletier <francois@jevalide.ca>
2024-06-17 16:04:57 -0400

    Add docker run for each system

---
+++ fastapi_backend/docker-run-macos.sh
@@ -0,0 +1,13 @@
+# Stop and remove existing containers
+
+docker stop fastapi_backend
+docker rm fastapi_backend
+
+# This program launches fastapi_backend in a local docker container.
+
+docker run -p 8000:8000 \
+  --env-file ".env" \
+  --name fastapi_backend \
+  --volume ./database:/app/database \
+  --network host \
+  local/fastapi-backend
--- fastapi_backend/docker-run-windows.ps1
+++ fastapi_backend/docker-run-windows.ps1
@@ -1,4 +1,6 @@
-#!/usr/bin/zsh
+# Create a new network if it doesn't exist
+
+docker network create medical-codex-net
 
 # Stop and remove existing containers
 
@@ -7,9 +9,9 @@ docker rm fastapi_backend
 
 # This program launches fastapi_backend in a local docker container.
 
-docker run -p 8080:8080 \
+docker run -p 8000:8000 \
   --env-file ".env" \
   --name fastapi_backend \
   --volume ./database:/app/database \
-  --network host \
+  --network medical-codex-net \
   local/fastapi-backend
