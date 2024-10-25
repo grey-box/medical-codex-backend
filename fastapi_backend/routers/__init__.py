d871fd963dbb323fe0ffb23e6acd3ea835abc902
Thayer Picart <thayer.picart@grey-box.ca>
2024-10-25 18:21:03 -0400

    chore: linked last_resort router to main router

--- fastapi_backend/routers/__init__.py
+++ fastapi_backend/routers/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-
-from routers import fuzzymatching, language, translate
+from routers import fuzzymatching, language, translate, last_resort
 
 main_router = APIRouter()
 
 main_router.include_router(fuzzymatching.router)
 main_router.include_router(language.router)
 main_router.include_router(translate.router)
+main_router.include_router(last_resort.router)
