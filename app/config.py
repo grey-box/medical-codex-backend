a3d81cd4516af087bd613cddeafb846230cde099
François Pelletier <francois@jevalide.ca>
2024-11-14 23:17:42 -0500

    Mega refactoring to have only the backend app at the root of the repository. Many code enhancements and updates.

--- app/config.py
+++ app/config.py
@@ -6,7 +6,12 @@ LOGGER_NAME = "codex_backend_logger"
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DB_TYPE: str = "postgresql"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "postgres"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str
     LOGGING_LEVEL: str
     LOGGING_FORMAT: str
 
