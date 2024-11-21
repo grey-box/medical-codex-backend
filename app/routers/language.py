60cf2f0c0e2dc5f5ec20aa65f0d16e0b809b4bd4
HalfKraut <111702704+HalfKraut@users.noreply.github.com>
2024-11-21 01:19:56 -0700

    define get_languages to query and return available languages

--- app/routers/language.py
+++ app/routers/language.py
@@ -12,6 +12,32 @@ router = APIRouter(prefix="/languages", tags=["languages"])
 logger = logging.getLogger(LOGGER_NAME)
 
 
-@router.get("/", response_model=schemas.TranslationLanguageResult)
+@router.get("/", response_model=schemas.AvailableLanguages)
 def get_languages(db: Session = Depends(database.get_db)):
-    return db.query(models.LanguagePairs).all()
+    '''
+        Gets the languages currently available to translate works to
+        and from using the translate endpoint
+    '''
+    # Query to get the langauge pairs from a View in the DB
+    language_pairs = db.query(
+            models.LanguagePairs
+        ).order_by(
+            models.LanguagePairs.source_language.asc()
+        ).all()
+    
+    # Covert to pydantic models
+    avialable_languages = [
+        schemas.AvailableLanguageResult(
+            source_language=language.source_language,
+            target_language=language.target_language,
+        )
+        for language in language_pairs
+        ]
+
+    # Log the pairs we are returning
+    logger.info(f'\nLanguage pairs:\n{language_pairs}')
+    
+    # Translate to a list based on the pydantic schemas
+    return schemas.AvailableLanguages(translations=avialable_languages)
+
+    
