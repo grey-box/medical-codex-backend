4d9250dfd6a380bd807d82d81138d170fccf035c
HalfKraut <111702704+HalfKraut@users.noreply.github.com>
2024-11-21 01:18:34 -0700

    Add SQLAlchemy Model and Pydantic Schemas for available languages

--- app/schemas.py
+++ app/schemas.py
@@ -3,52 +3,50 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
-class FuzzyResult(BaseModel):
-    matching_name: str
-    matching_source: str
-    matching_uid: int
-
-
-class TranslationResult(BaseModel):
-    translated_name: str
-    translated_source: str
-    translated_uid: int
-
-
-# 3 in the diagram
-class FuzzyMatching(BaseModel):
-    results: List[FuzzyResult]
-
-
 # 2 in the diagram
 class FuzzyQuery(BaseModel):
     source_language: str
     query: str
     threshold: int = 10
     nb_max_results: int = 10
+class FuzzyResult(BaseModel):
+    matching_name: str
+    matching_source: str
+    matching_uid: int
 
+# 3 in the diagram
+class FuzzyMatching(BaseModel):
+    results: List[FuzzyResult]
 
-class TranslationLanguagePair(BaseModel):
-    source_language: str
-    target_language: str
-
-    class Config:
-        from_attributes = True
-
-
-class TranslationLanguageResult(BaseModel):
-    translations: List[TranslationLanguagePair]
-
-
-# 6 in the diagram
-class Translation(BaseModel):
-    results: List[TranslationResult]
 
 
 # 5 in the diagram
 class TranslationQuery(BaseModel):
     translation_query: FuzzyResult
     target_language: str
+
+class TranslationResult(BaseModel):
+    translated_name: str
+    translated_source: str
+    translated_uid: int
+
+# 6 in the diagram
+class Translation(BaseModel):
+    results: List[TranslationResult]
+
+
+
+class AvailableLanguageResult(BaseModel):
+    source_language: str
+    target_language: str
+
+    class Config:
+        from_attributes = True
+
+class AvailableLanguages(BaseModel):
+    translations: List[AvailableLanguageResult]
+
+
 
 
 class UniqueTranslationsPYD(BaseModel):
